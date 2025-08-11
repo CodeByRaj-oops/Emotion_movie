a27f05efd6ed03fe9d7e3a04d86e98d6a0d54535
CodeByRaj-oops <anandraj48073@gmail.com>
2025-08-11 20:48:35 +0530

    add emotion_detection

--- backend/app/camera.py
+++ backend/app/camera.py
@@ -1,25 +1,33 @@
+
 import cv2
 
 def open_webcam():
-    # Open the default camera
+    
     cap = cv2.VideoCapture(0)
 
     if not cap.isOpened():
-        print("Error: Could not open camera.")
+        print("❌ Error: Could not open webcam.")
         return
-    
+
+    print("✅ Press 'q' to quit webcam preview.")
+
     while True:
         ret, frame = cap.read()
         if not ret:
-            print("Error: Could not read frame.")
+            print("❌ Error: Failed to read frame.")
             break
+        
+       # Flip the frame horizontally (mirror effect)
+        frame = cv2.flip(frame, 1)
 
         # Show the frame in a window
         cv2.imshow("Webcam Feed", frame)
 
+        # Exit when 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    
+
+    # Release resources
     cap.release()
     cv2.destroyAllWindows()
 
